846532177534f278d3115399c3e7ecbcf7e659e4
omar-batrawy <obatrawy7@gmail.com>
2023-12-05 15:26:22 +0200

    .

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './Hero';
-import { Heros } from './mock-heroes';
+
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'https://dummyjson.com/products';
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(Heros);
-    this.MessageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-  constructor(private MessageService: MessageService) {}
+      // TODO: better job of transforming error for user consumption
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 }
