7b5f2e1aa20fb9bce292a72fd18a45a83179399c
omar-batrawy <obatrawy7@gmail.com>
2023-12-05 11:24:44 +0200

    .

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Hero } from './Hero';
 import { Heros } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(Heros);
+    this.MessageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() {}
+  constructor(private MessageService: MessageService) {}
 }
