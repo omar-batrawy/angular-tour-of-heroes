ba8aa8ff707420e293ab06894a3a2338a0ee36bd
omar-batrawy <obatrawy7@gmail.com>
2023-12-05 15:26:17 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,25 @@ import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MessagesComponent } from './messages/messages.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { InputtestComponent } from './inputtest/inputtest.component';
+import { MatButtonModule } from '@angular/material/button';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkDialogStylingExampleDialogComponent } from './inputtest/cdk-dialog-styling-example-dialog.component';
+import { DialogModule } from '@angular/cdk/dialog';
 @NgModule({
-  declarations: [AppComponent, HerosComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    HerosComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    InputtestComponent,
+    CdkDialogStylingExampleDialogComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -18,6 +35,14 @@ import { MessagesComponent } from './messages/messages.component';
     NgFor,
     UpperCasePipe,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    TextFieldModule,
+    MatButtonModule,
+    ScrollingModule,
+    DialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
