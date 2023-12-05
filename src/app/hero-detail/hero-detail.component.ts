e773ca837d5d05b54deb3b0556ba109a7f0b4f20
omar-batrawy <obatrawy7@gmail.com>
2023-12-05 10:28:12 +0200

    .

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Hero } from '../Hero';
+import { Heros } from '../mock-heroes';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
+
