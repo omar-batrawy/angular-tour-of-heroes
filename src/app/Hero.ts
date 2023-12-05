c30c6d7b1087073fd1660338f14bbd6f1b28c2ed
omar-batrawy <obatrawy7@gmail.com>
2023-12-05 15:26:37 +0200

    .

--- src/app/Hero.ts
+++ src/app/Hero.ts
@@ -1,4 +1,13 @@
 export interface Hero {
   id: number;
-  name: string;
+  title: string;
+  description: string;
+  price: number; // Changed from string to number
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
 }
